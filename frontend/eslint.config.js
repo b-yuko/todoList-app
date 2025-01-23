33837ea4f2466c60930dd2ac7b4ed5f61e3e5c4f
b-yuko <yuko.banno.oo@gmail.com>
2025-01-23 18:48:33 +0900

    setup: eslint-plugin-jsx-a11y の導入と設定をした

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -4,11 +4,16 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import react from 'eslint-plugin-react'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      jsxA11y.flatConfigs.recommended
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
