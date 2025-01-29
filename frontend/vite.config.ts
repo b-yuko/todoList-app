f965d384c62790d93c0ea560e4deb93d14c20abc
b-yuko <yuko.banno.oo@gmail.com>
2025-01-29 16:30:00 +0900

    setup: モジュールのインポートパスを短縮する設定をした

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,7 @@
 /// <reference types="vitest/config" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'node:path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,5 +10,10 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     setupFiles: ['./setupTests.ts'],
-  }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
