d3fd91ba0ebfec01dd86eab272253f43d1310660
b-yuko <yuko.banno.oo@gmail.com>
2025-03-07 20:47:37 +0900

    chore: ディレクトリ構成の変更

--- frontend/src/repository/TaskRepository.ts
+++ frontend/src/repository/TaskRepository.ts
@@ -7,14 +7,7 @@ export interface TaskRepository {
 export class TaskRepositoryImpl implements TaskRepository {
   async saveTask(task: string): Promise<void> {
     try {
-      const response = await axios.post("api/task", task);
-
-      // TODO: 最終的に削除するコード
-      // --- 開発中のみ ↓ ---
-      if (response.status === 200) {
-        console.log("200 OK: taskRepository.saveTask(task)");
-      }
-      // --- 開発中のみ ↑ ---
+      await axios.post("api/task", task);
     } catch (error) {
       console.log(error);
     }
