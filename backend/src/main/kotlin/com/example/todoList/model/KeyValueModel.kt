6cd3c4e4d2fff0baab8157ca06e6633b43a4ec05
b-yuko <yuko.banno.oo@gmail.com>
2025-03-18 08:13:57 +0900

    chore: backend 削除

--- backend/src/main/kotlin/com/example/todoList/model/KeyValueModel.kt
+++
@@ -1,7 +0,0 @@
-package com.example.todoList.model
-
-data class KeyValueModel(
-    var pk: String = "",
-    var sk: String = "",
-    var taskName: String = "",
-)
--- backend/src/main/kotlin/com/example/todoList/repository/KeyValueRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.example.todoList.repository
-
-import com.example.todoList.model.KeyValueModel
-
-interface KeyValueRepository {
-    fun save(model: KeyValueModel)
-}
