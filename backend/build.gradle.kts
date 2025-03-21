24f3dabff85278345d992251cd444c33abf384c2
b-yuko <yuko.banno.oo@gmail.com>
2025-03-21 19:30:28 +0900

    fix: 依存関係のバージョンを最新に更新した

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,9 +1,9 @@
 val mockitoAgent: Configuration by configurations.creating
 
 plugins {
-    kotlin("jvm") version "1.9.25"
-    kotlin("plugin.spring") version "1.9.25"
-    id("org.springframework.boot") version "3.4.3"
+    kotlin("jvm") version "2.1.20"
+    kotlin("plugin.spring") version "2.1.20"
+    id("org.springframework.boot") version "3.4.4"
     id("io.spring.dependency-management") version "1.1.7"
     id("org.jlleitschuh.gradle.ktlint") version "12.2.0"
 }
